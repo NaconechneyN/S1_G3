c5434b41c2ab0a4e6e7f186abd7e4a80b258ceec
StefanoFabbro <stefano.fabbro@sancorsalud.com.ar>
2023-03-03 15:05:09 -0300

    agregando el service

---
+++ Spring1/src/main/java/com/Bootcamp/Spring1/repository/HotelRepository.java
@@ -0,0 +1,7 @@
+package com.Bootcamp.Spring1.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HotelRepository {
+}
--- Spring1/src/main/java/com/Bootcamp/Spring1/service/HotelService.java
+++ Spring1/src/main/java/com/Bootcamp/Spring1/service/HotelService.java
@@ -1,4 +1,7 @@
 package com.Bootcamp.Spring1.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HotelService {
 }
