9599d33ce52e080eebb53260fde0126a6325d57b
StefanoFabbro <stefano.fabbro@sancorsalud.com.ar>
2023-03-07 16:41:36 -0300

    Requerimiento 5 Terminado, mergeado

--- Spring1/src/main/java/com/Bootcamp/Spring1/controller/FlyController.java
+++ Spring1/src/main/java/com/Bootcamp/Spring1/controller/FlyController.java
@@ -6,17 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-<<<<<<< HEAD
-
-import java.time.LocalDate;
-=======
 import com.Bootcamp.Spring1.dto.request.fly.FlyRequestDTO;
-import com.Bootcamp.Spring1.dto.request.hotel.HotelRequestDTO;
 import com.Bootcamp.Spring1.dto.response.FlyResponseDTO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
->>>>>>> 29cb53f51923369a84ee82d47fd840ce5f507203
+
+
 import java.util.List;
 
 
