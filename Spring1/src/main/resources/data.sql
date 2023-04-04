1db0bb278ccfea8a42b1ea5a2b4738852e0b68a8
StefanoFabbro <stefano.fabbro@sancorsalud.com.ar>
2023-04-04 08:12:26 -0300

    agregando hoteles al data sql

--- Spring1/src/main/resources/data.sql
+++ Spring1/src/main/resources/data.sql
@@ -1,2 +1,36 @@
 INSERT INTO hotel (codigo_hotel, nombre_hotel, ciudad, tipo_habitacion, precio, fecha_disponibilidad_desde, fecha_disponibilidad_hasta, reservado)
-VALUES ('CH-0002', 'Cataratas Hotel', 'Puerto Iguazú', 'doble', 6300.00, '2023/04/10', '2023/05/20', false);
+VALUES ('CH-0002', 'Cataratas Hotel', 'Puerto Iguazú', 'doble', 6300.00, '2023/04/10', '2023/06/20', false);
+
+INSERT INTO hotel (codigo_hotel, nombre_hotel, ciudad, tipo_habitacion, precio, fecha_disponibilidad_desde, fecha_disponibilidad_hasta, reservado)
+VALUES ('CH-0003', 'Cataratas Hotel 2', 'Puerto Iguazú', 'triple', 8300.00, '2023/04/10', '2023/05/23', false);
+
+INSERT INTO hotel (codigo_hotel, nombre_hotel, ciudad, tipo_habitacion, precio, fecha_disponibilidad_desde, fecha_disponibilidad_hasta, reservado)
+VALUES ('HB-0001', 'Hotel Bristol', 'Buenos Aires', 'single', 5435.00, '2023/02/10', '2023/06/19', false);
+
+INSERT INTO hotel (codigo_hotel, nombre_hotel, ciudad, tipo_habitacion, precio, fecha_disponibilidad_desde, fecha_disponibilidad_hasta, reservado)
+VALUES ('BH-0002', 'Hotel Bristol 2', 'Buenos Aires', 'doble', 7200.00, '2023/02/12', '2023/06/17', false);
+
+INSERT INTO hotel (codigo_hotel, nombre_hotel, ciudad, tipo_habitacion, precio, fecha_disponibilidad_desde, fecha_disponibilidad_hasta, reservado)
+VALUES ('SH-0002', 'Sheraton', 'Tucumán', 'doble', 5790.00, '2023/02/12', '2023/06/17', false);
+
+INSERT INTO hotel (codigo_hotel, nombre_hotel, ciudad, tipo_habitacion, precio, fecha_disponibilidad_desde, fecha_disponibilidad_hasta, reservado)
+VALUES ('SH-0001', 'Sheraton 2', 'Tucumán', 'single', 4150.00, '2023/02/12', '2023/06/17', false);
+
+INSERT INTO hotel (codigo_hotel, nombre_hotel, ciudad, tipo_habitacion, precio, fecha_disponibilidad_desde, fecha_disponibilidad_hasta, reservado)
+VALUES ('SE-0001', 'Selina', 'Bogotá', 'single', 3900.00, '2023/02/12', '2023/06/17', false);
+
+INSERT INTO hotel (codigo_hotel, nombre_hotel, ciudad, tipo_habitacion, precio, fecha_disponibilidad_desde, fecha_disponibilidad_hasta, reservado)
+VALUES ('SE-0002', 'Selina 2', 'Bogotá', 'doble', 5840.00, '2023/02/12', '2023/06/17', false);
+
+INSERT INTO hotel (codigo_hotel, nombre_hotel, ciudad, tipo_habitacion, precio, fecha_disponibilidad_desde, fecha_disponibilidad_hasta, reservado)
+VALUES ('EC-0003', 'El Campín', 'Bogotá', 'triple', 7020.00, '2023/02/12', '2023/06/17', false);
+
+INSERT INTO hotel (codigo_hotel, nombre_hotel, ciudad, tipo_habitacion, precio, fecha_disponibilidad_desde, fecha_disponibilidad_hasta, reservado)
+VALUES ('CP-0004', 'Central Plaza', 'Medellín', 'Múltiple', 8600.00, '2023/02/12', '2023/06/17', false);
+
+INSERT INTO hotel (codigo_hotel, nombre_hotel, ciudad, tipo_habitacion, precio, fecha_disponibilidad_desde, fecha_disponibilidad_hasta, reservado)
+VALUES ('CP-0002', 'Central Plaza 2', 'Medellín', 'doble', 6400.00, '2023/02/12', '2023/06/17', false);
+
+INSERT INTO hotel (codigo_hotel, nombre_hotel, ciudad, tipo_habitacion, precio, fecha_disponibilidad_desde, fecha_disponibilidad_hasta, reservado)
+VALUES ('BG-0004', 'Bocagrande', 'Cartagena', 'Múltiple', 9370.00, '2023/02/12', '2023/06/17', false);
+
